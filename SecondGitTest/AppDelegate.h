69e99cb402dabf00297705e9064f617877d1daf8
EileenLeung <eileenelement@gmail.com>
2013-12-28 19:53:00 +0800

    Initial Commit

---
+++ SecondGitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SecondGitTest
+//
+//  Created by Eileen on 28/12/13.
+//  Copyright (c) 2013 Eileen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
